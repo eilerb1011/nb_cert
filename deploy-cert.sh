058eed9d895283564951b27c85f650679e4a87c8
eilerb1011 <133176828+eilerb1011@users.noreply.github.com>
2024-06-27 15:01:11 -0400

    Update deploy-cert.sh

--- deploy-cert.sh
+++ deploy-cert.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-token=
+###Put your token with DNS read/write here
+dns_token=
+#####Place your host fqdn here####
 fqdn=
-certbot certonly --manual --manual-auth-hook /etc/letsencrypt/acme-lindns-auth.py --preferred-challenges dns --debug-challenges -d $fqdn 
+#####Fill in your token with Nodebalancer Read Write here#####
+nb_token= 
+####Fill in your Nodebalancer ID here####
+nb_id=
+export dns_token
+export fqdn
+export nb_token
+export nb_id
+cd /tmp
+git clone https://github.com/eilerb1011/nb_cert
+chmod +x /tmp/nb_cert/deploy-cert.sh
+chmod +x /tmp/nb_cert/nb-cert.sh
+chmod +x /tmp/nb_cert/acme-lin-dns.py
+apt update && apt install -y certbot jq python3
+mv /tmp/nb_cert/acme-lin-dns.py /etc/letsencrypt/
+mv /tmp/nb_cert/nb-cert.sh /etc/letsencrypt/
+certbot certonly --manual --manual-auth-hook /etc/letsencrypt/acme-lindns-auth.py --preferred-challenges dns -d $fqdn 
 nb-cert.sh
